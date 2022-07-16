f90c2115db85cf9c3035c5185d946271e84aafe2
rogue-towne <69937144+rogue-towne@users.noreply.github.com>
2022-07-15 21:37:34 -0600

    add auth to market routes

--- src/routes/market.ts
+++ src/routes/market.ts
@@ -1,13 +1,14 @@
 import express from 'express'
 const router = express.Router();
+import { requiresAuth } from 'express-openid-connect';
 import { deleteMarketHandler, getAllHandler, getSingleHandler, postNewMarketHandler, putUpdateMarketHandler } from '../controllers/market_controller';
 import validate from '../helper/validate';
 import { deleteMarketSchema, getSingleSchema, postNewMarketSchema, putUpdateMarketSchema } from '../schema/market_schema';
 
-router.post('/', validate(postNewMarketSchema), postNewMarketHandler);
-router.delete('/:marketId', validate(deleteMarketSchema), deleteMarketHandler);
-router.get('/:marketId', validate(getSingleSchema), getSingleHandler);
-router.get('/', getAllHandler);
-router.put('/:marketId', validate(putUpdateMarketSchema), putUpdateMarketHandler);
+router.post('/', requiresAuth(), validate(postNewMarketSchema), postNewMarketHandler);
+router.delete('/:marketId', requiresAuth(), validate(deleteMarketSchema), deleteMarketHandler);
+router.get('/:marketId', requiresAuth(), validate(getSingleSchema), getSingleHandler);
+router.get('/', requiresAuth(), getAllHandler);
+router.put('/:marketId', requiresAuth(), validate(putUpdateMarketSchema), putUpdateMarketHandler);
 
 export default router;
