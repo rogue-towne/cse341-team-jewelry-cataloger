07fa882967ff8ee34ab7f7572faf9dda36db1a44
rogue-towne <69937144+rogue-towne@users.noreply.github.com>
2022-07-15 22:14:45 -0600

    remove extra modules

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response} from 'express'
+import express from 'express'
 import userRouter from './user'
 import insuranceRouter from './insurance'
 import jewelryRouter from './jewelry'
