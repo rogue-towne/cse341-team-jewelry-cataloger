1986cd0d557a7808b39b15d3060c83245f844566
rogue-towne <69937144+rogue-towne@users.noreply.github.com>
2022-07-15 21:24:34 -0600

    fix user routes and delete services

--- src/controllers/user_controller.ts
+++ src/controllers/user_controller.ts
@@ -1,28 +1,29 @@
 import {Request, Response} from 'express'
-import { GetSingleUserInput, PostNewUserInput, PutUpdateUserInput } from '../schema/user_schema';
-import { deleteUser, getAll, getSingle, postNewUser, putUpdateUser } from '../services/user_service'
-
+import UserDataModel from '../models/user_model';
+import { DeleteUserInput, GetSingleUserInput, PostNewUserInput, PutUpdateUserInput } from '../schema/user_schema';
 
 export async function getAllHandler(req: Request, res: Response){
     try {
-        const users = await getAll();
+        const users = await UserDataModel.find();
         return res.status(200).json(users)
     } catch (err: any) {
+        res.status(401).json({message: err.message})
         res.status(503).json({message: err.message})
+        res.status(400).json({message: err.message}) 
     }
 }
 export async function getSingleHandler(req: Request<GetSingleUserInput['params']>, res: Response){
-    const userId = req.params.userId;
-    const user = await getSingle({userId})
-    if (!user){
-        return res.sendStatus(404)
+    try { const user = await UserDataModel.findById(req.params.userId)
+        return res.status(200).json(user)
+    } catch {
+        res.status(400).json("Invalid ID")
+        res.status(500).json("Internal Server Error")
     }
-    return res.status(200).json(user)
 }
 export async function postNewUserHandler(req: Request<{}, {}, PostNewUserInput['body']>, res: Response){
     try {
-        const user = await postNewUser(req.body);
-        return res.status(200).json(user)
+        const user = await UserDataModel.create(req.body);
+        return res.status(201).json(user)
     } catch (err: any) {
         res.status(401).json({message: err.message})
         res.status(503).json({message: err.message})
@@ -30,21 +31,21 @@ export async function postNewUserHandler(req: Request<{}, {}, PostNewUserInput['
     }
 }
 export async function putUpdateUserHandler(req: Request<PutUpdateUserInput['params']>, res: Response){
-    const userId = req.params.userId;
-    const user = await getSingle({userId})
-    if (!user){
-        return res.sendStatus(404)
+    try { 
+        await UserDataModel.findByIdAndUpdate(req.params.userId, req.body)
+        res.send("Updated User");
+    } catch {
+        res.status(400).json("Invalid ID")
+        res.status(500).json("Internal Server Error")
     }
-    const update = req.body
-    const updatedUser = await putUpdateUser({userId}, update, {new: true})
-    return res.status(200).json(updatedUser)
 }
-export async function deleteUserHandler(req: Request<PutUpdateUserInput['params']>, res: Response){
-    const userId = req.params.userId;
-    const user = await getSingle({userId})
-    if (!user){
-        return res.sendStatus(404)
-    }
-    await deleteUser({userId});
-    return res.sendStatus(200)
+export async function deleteUserHandler(req: Request<DeleteUserInput['params']>, res: Response){
+    try { 
+        const user = await UserDataModel.findById(req.params.userId)
+        user?.deleteOne()
+        res.json('Deleted User')
+    } catch {
+        res.status(400).json("Invalid ID")
+        res.status(500).json("Internal Server Error")
+    };
 }
